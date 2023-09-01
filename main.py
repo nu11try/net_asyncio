9ee2a2fcc19703eef11b7f3fdc3df7adcf3e431c
Zuev Vladislav <v.zuev@jump.finance>
2023-09-01 14:10:33 +0300

    Fix

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
-import aiohttp
 import asyncio
-import requests
 import sqlite3
+import aiohttp
+import requests
+
+from aiohttp import ContentTypeError
 
 
-async def fetch_character(session, character_id):
-    url = f'https://swapi.dev/api/people/{character_id}/'
+async def fetch_any(session, url, field=None):
     async with session.get(url) as response:
-        return await response.json()
-
+        try:
+            res = await response.json()
+            return res if field is None else res[field]
+        except ContentTypeError:
+            return None
 
 async def load_character_data(session, conn, character_id):
-    character_data = await fetch_character(session, character_id)
-    if character_data.get('url'):
-        cursor = conn.cursor()
-        cursor.execute('''
-            INSERT INTO characters (birth_year, eye_color, films, gender, hair_color, height, homeworld, mass, name, skin_color, species, starships, vehicles)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ''', (
-            character_data['birth_year'],
-            character_data['eye_color'],
-            ','.join(character_data['films']),
-            character_data['gender'],
-            character_data['hair_color'],
-            character_data['height'],
-            character_data['homeworld'],
-            character_data['mass'],
-            character_data['name'],
-            character_data['skin_color'],
-            ','.join(character_data['species']),
-            ','.join(character_data['starships']),
-            ','.join(character_data['vehicles'])
-        ))
-        conn.commit()
+    character_data = await fetch_any(session, f'https://swapi.dev/api/people/{character_id}/')
+
+    if character_data is not None:
+        if character_data.get('url'):
+            films = [await fetch_any(session, el, 'title') for el in character_data['films']]
+            species = [await fetch_any(session, el, 'name') for el in character_data['species']]
+            starships = [await fetch_any(session, el, 'name') for el in character_data['starships']]
+            vehicles = [await fetch_any(session, el, 'name') for el in character_data['vehicles']]
+            homeworld = await fetch_any(session, character_data['homeworld'], 'name')
+
+            cursor = conn.cursor()
+            cursor.execute('''
+                INSERT INTO characters (birth_year, eye_color, films, gender, hair_color, height, homeworld, mass, name, skin_color, species, starships, vehicles)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (
+                character_data['birth_year'],
+                character_data['eye_color'],
+                ', '.join(films),
+                character_data['gender'],
+                character_data['hair_color'],
+                character_data['height'],
+                homeworld,
+                character_data['mass'],
+                character_data['name'],
+                character_data['skin_color'],
+                ', '.join(species),
+                ', '.join(starships),
+                ', '.join(vehicles)
+            ))
+            conn.commit()
 
 
 async def main():
